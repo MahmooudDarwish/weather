47505d9112b236b858598a1ef02dc5d987018fad
pc <mahmoudsaaddarwish1@gmail.com>
2024-09-26 03:50:39 +0300

    Add post notification permission

--- app/src/main/java/com/example/weather/features/alarm/services/NotificationWorker.kt
+++ app/src/main/java/com/example/weather/features/alarm/services/NotificationWorker.kt
@@ -1,7 +1,6 @@
 package com.example.weather.features.alarm.services
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
